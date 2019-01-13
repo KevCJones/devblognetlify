952f5a3038054785eac2ef0ddc7e536ac6fc4197
Kevin Jones <kevin.jones@pushdr.com>
2019-01-13 23:29:39 +0000

    reconfigure

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
-  primaryColor: '#3498db', // Required
+  title: "My coding diary", // Required
+  author: "Kevin Jones", // Required
+  description: "Frontend Engineer",
+  primaryColor: "#3498db", // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: "https://www.kevincjones.co.uk",
+    github: "https://github.com/kevcjones",
+    twitter: "https://twitter.com/kevcjones",
+    linkedin: "https://www.linkedin.com/in/kevin-c-jones-7a22372/"
   },
-  pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  pathPrefix: "",
+  siteUrl: "https://kevincjones.co.uk"
 };
